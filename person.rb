088c352f5b2a14d6413f04e439278dbf128b2547
fernando-silvabr66 <fernando.silvabr66@gmail.com>
2022-05-20 20:54:08 -0300

    Add decorator features

--- person.rb
+++ person.rb
@@ -1,3 +1,5 @@
+require './nameable'
+
 # class for person data
 class Person
   # class for person data
@@ -5,10 +7,15 @@ class Person
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def of_age?
