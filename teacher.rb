415e6eb86e0395deb8c76ead75e55f79fac01431
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-05-21 18:22:07 -0300

    Fix linter error #5

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require './person'
 # class for teacher data
 class Teacher < Person
   # class for student data
-  def initialize(specialization, age, name = 'Unknown', parent_permission = true)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
